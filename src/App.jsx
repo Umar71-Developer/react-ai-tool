47bc9e8b978b47debe301c69286d89705fccc239
Umar71-Developer <141718806+Umar71-Developer@users.noreply.github.com>
2025-06-05 17:54:16 +0500

    step 10 is completed and recent history styling applied and clear history button is made and working properly

--- src/App.jsx
+++ src/App.jsx
@@ -48,13 +48,22 @@ function App() {
     ]);
   }
   // console.log(result)
+const clearHistory = () => {
+  localStorage.clear();
+  setRecentHistory([])
+}
 
   return (
     <div className="grid grid-cols-5 h-screen text-center">
       <div className="col-span-1 bg-zinc-700">
-        <ul>
+        <h1 className="text-lg text-white pt-3 flex justify-center items-center"> <span>Recent History</span>
+
+          <button onClick={clearHistory} className="cursor-pointer"><svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#1f1f1f"><path d="M312-144q-29.7 0-50.85-21.15Q240-186.3 240-216v-480h-48v-72h192v-48h192v48h192v72h-48v479.57Q720-186 698.85-165T648-144H312Zm336-552H312v480h336v-480ZM384-288h72v-336h-72v336Zm120 0h72v-336h-72v336ZM312-696v480-480Z"/></svg></button>
+        </h1>
+        
+        <ul className="text-left overflow-auto">
         {recentHistory && recentHistory.map((item , index) => 
-        <li key={index+Math.random()}>{item}</li>
+        <li className="p-0.5 pl-5 cursor-pointer text-zinc-400 hover:bg-zinc-800 hover:text-zinc-200 truncate" key={index+Math.random()}>{item}</li>
         )}
         </ul>
       </div>
