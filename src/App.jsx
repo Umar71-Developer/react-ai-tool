8e90faa33a7923657821ed9fadcffba73367ab6d
Umar71-Developer <141718806+Umar71-Developer@users.noreply.github.com>
2025-06-05 19:40:11 +0500

    step 12 is completed where a functionality is applied on recent history where i can click on history and it will seach again

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { URL } from "./constants";
 import Answer from "./components/Answers";
@@ -6,32 +6,42 @@ import Answer from "./components/Answers";
 function App() {
   const [question, setQuestion] = useState("");
   const [result, setResult] = useState([]);
-  const [recentHistory, setRecentHistory] = useState(JSON.parse(localStorage.getItem('history')));
+  const [recentHistory, setRecentHistory] = useState(
+    JSON.parse(localStorage.getItem("history"))
+  );
+  const [selectedHistory, setSelectedHistory] = useState("");
+
+
+  async function askQuestion() {
+    if (!question && !selectedHistory) {
+      return false;
+    }
+    if (question) {
+      if (localStorage.getItem("history")) {
+        let history = JSON.parse(localStorage.getItem("history"));
+        history = [question, ...history];
+        localStorage.setItem("history", JSON.stringify(history));
+        setRecentHistory(history);
+      } else {
+        localStorage.setItem("history", JSON.stringify([question]));
+        setRecentHistory([question]);
+      }
+    }
+
+      const payloadData = question ? question : selectedHistory;
 
   const payload = {
     contents: [
       {
         parts: [
           {
-            text: question,
+            text: payloadData,
           },
         ],
       },
     ],
   };
-  async function askQuestion() {
-    if(!question){
-      return false
-    }
-    if (localStorage.getItem('history')) {
-      let history = JSON.parse(localStorage.getItem('history'))
-      history = [question, ...history]
-      localStorage.setItem('history', JSON.stringify(history));
-      setRecentHistory(history)
-    } else {
-      localStorage.setItem('history', JSON.stringify([question]));
-      setRecentHistory([question])
-    }
+
 
     // console.log(recentHistory)
     let response = await fetch(URL, {
@@ -46,37 +56,59 @@ function App() {
     //  console.log(dataString)
     setResult([
       ...result,
-      { type: "q", text: question },
+      { type: "q", text: question ? question : selectedHistory },
       { type: "a", text: dataString },
     ]);
-    setQuestion('')
+    setQuestion("");
   }
   // console.log(result)
-const clearHistory = () => {
-  localStorage.clear();
-  setRecentHistory([])
-}
+  const clearHistory = () => {
+    localStorage.clear();
+    setRecentHistory([]);
+  };
 
-const isEnter = (event) => {
-console.log(event.key)
-if(event.key == "Enter"){
-  askQuestion();
+  const isEnter = (event) => {
+    console.log(event.key);
+    if (event.key == "Enter") {
+      askQuestion();
+    }
+  };
 
-}
-}
+  useEffect(() => {
+    console.log(selectedHistory);
+    askQuestion()
+  }, [selectedHistory]);
 
   return (
     <div className="grid grid-cols-5 h-screen text-center">
       <div className="col-span-1 bg-zinc-700">
-        <h1 className="text-lg text-white pt-3 flex justify-center items-center"> <span>Recent History</span>
-
-          <button onClick={clearHistory} className="cursor-pointer"><svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#1f1f1f"><path d="M312-144q-29.7 0-50.85-21.15Q240-186.3 240-216v-480h-48v-72h192v-48h192v48h192v72h-48v479.57Q720-186 698.85-165T648-144H312Zm336-552H312v480h336v-480ZM384-288h72v-336h-72v336Zm120 0h72v-336h-72v336ZM312-696v480-480Z"/></svg></button>
+        <h1 className="text-lg text-white pt-3 flex justify-center items-center">
+          {" "}
+          <span>Recent History</span>
+          <button onClick={clearHistory} className="cursor-pointer">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="20px"
+              viewBox="0 -960 960 960"
+              width="20px"
+              fill="#1f1f1f"
+            >
+              <path d="M312-144q-29.7 0-50.85-21.15Q240-186.3 240-216v-480h-48v-72h192v-48h192v48h192v72h-48v479.57Q720-186 698.85-165T648-144H312Zm336-552H312v480h336v-480ZM384-288h72v-336h-72v336Zm120 0h72v-336h-72v336ZM312-696v480-480Z" />
+            </svg>
+          </button>
         </h1>
-        
+
         <ul className="text-left overflow-auto">
-        {recentHistory && recentHistory.map((item , index) => 
-        <li className="p-0.5 pl-5 cursor-pointer text-zinc-400 hover:bg-zinc-800 hover:text-zinc-200 truncate" key={index+Math.random()}>{item}</li>
-        )}
+          {recentHistory &&
+            recentHistory.map((item, index) => (
+              <li
+                onClick={() => setSelectedHistory(item)}
+                className="p-0.5 pl-5 cursor-pointer text-zinc-400 hover:bg-zinc-800 hover:text-zinc-200 truncate"
+                key={index + Math.random()}
+              >
+                {item}
+              </li>
+            ))}
         </ul>
       </div>
       <div className="col-span-4 ">
